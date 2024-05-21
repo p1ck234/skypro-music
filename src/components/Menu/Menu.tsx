a55a34e001ce12b920ebfde69bb0445ffd24405b
p1ck23 <danil-vetrov47@mail.ru>
2024-05-21 22:42:05 +0400

    burger menu functional

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,8 +1,16 @@
+"use client"
 import Link from "next/link";
 import styles from "./Menu.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 export const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toogleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={styles.mainNav}>
       <div className={styles.navLogo}>
@@ -14,30 +22,32 @@ export const Menu = () => {
           src="/img/logo.png"
         />
       </div>
-      <div className={styles.navBurger}>
+      <div className={styles.navBurger} onClick={toogleMenu}>
         <span className={styles.burgerLine}></span>
         <span className={styles.burgerLine}></span>
         <span className={styles.burgerLine}></span>
       </div>
-      <div className={styles.navMenu}>
-        <ul className={styles.menuList}>
-          <li className={styles.menuItem}>
-            <a href="#" className={styles.menuLink}>
-              Главное
-            </a>
-          </li>
-          <li className={styles.menuItem}>
-            <a href="#" className={styles.menuLink}>
-              Мой плейлист
-            </a>
-          </li>
-          <li className={styles.menuItem}>
-            <Link href="/singin" className={styles.menuLink}>
-              Войти
-            </Link>
-          </li>
-        </ul>
-      </div>
+      {isOpen && (
+        <div className={styles.navMenu}>
+          <ul className={styles.menuList}>
+            <li className={styles.menuItem}>
+              <a href="#" className={styles.menuLink}>
+                Главное
+              </a>
+            </li>
+            <li className={styles.menuItem}>
+              <a href="#" className={styles.menuLink}>
+                Мой плейлист
+              </a>
+            </li>
+            <li className={styles.menuItem}>
+              <Link href="/singin" className={styles.menuLink}>
+                Войти
+              </Link>
+            </li>
+          </ul>
+        </div>
+      )}
     </nav>
   );
 };
