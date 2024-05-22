fbcfbe22ee7363d89857cf554ded56b4426301c9
p1ck23 <danil-vetrov47@mail.ru>
2024-05-22 21:18:16 +0400

    create tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Логирование ошибки
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div>
+      <h2>Что-то пошло не так!</h2>
+      <button onClick={reset}>Попробовать снова</button>
+    </div>
+  );
+}
