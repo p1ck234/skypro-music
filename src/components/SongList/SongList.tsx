3c27588521945add5a1fef29c86ed88534cf6e72
p1ck23 <danil-vetrov47@mail.ru>
2024-05-23 20:42:22 +0400

    complited api html

--- src/components/SongList/SongList.tsx
+++ src/components/SongList/SongList.tsx
@@ -1,22 +1,27 @@
-import { Search } from "../Search/Search";
-import styles from "./SongList.module.css";
-import clsx from "clsx";
-import { getTracks } from "@/api/api";
+import { FC } from "react";
 import { TrackType } from "@/types/types";
+import clsx from "clsx";
+import styles from "./SongList.module.css";
+import { Search } from "../Search/Search";
 import { Sorting } from "../Sorting/Sorting";
 
-export const SongList = async () => {
-  const tracks: TrackType[] = await getTracks();
-  const uniqueAuthors = Array.from(
-    new Set(tracks.map((track) => track.author))
-  );
-  console.log(uniqueAuthors);
+type Props = {
+  tracks: TrackType[];
+};
+
+const formatDuration = (seconds: number) => {
+  const minutes = Math.floor(seconds / 60);
+  const remainingSeconds = seconds % 60;
+  return `${minutes}:${remainingSeconds < 10 ? "0" : ""}${remainingSeconds}`;
+};
+
+export const SongList: FC<Props> = ({ tracks }) => {
   return (
     <div>
       <div className={clsx(styles.mainSongList, styles.songList)}>
         <Search />
         <h2 className={styles.songListHeading2}>Треки</h2>
-        <Sorting />
+        <Sorting tracks={tracks} />
         <div className={clsx(styles.songListContent, styles.playlistContent)}>
           <div className={clsx(styles.contentTitle, styles.playlistTitle)}>
             <div className={clsx(styles.playlistTitleCol, styles.col01)}>
@@ -30,51 +35,49 @@ export const SongList = async () => {
             </div>
             <div className={clsx(styles.playlistTitleCol, styles.col04)}>
               <svg className={styles.playlistTitleSvg}>
-                <use xlinkHref="img/icon/sprite.svg#icon-watch"></use>
+                <use xlinkHref="/img/icon/sprite.svg#icon-watch"></use>
               </svg>
             </div>
           </div>
 
           <div className={clsx(styles.contentPlaylist, styles.playlist)}>
-            {tracks.map((track: TrackType, index: number) => {
-              return (
-                <div className={styles.playlistItem} key={index}>
-                  <div className={clsx(styles.playlistTrack, styles.track)}>
-                    <div className={styles.trackTitle}>
-                      <div className={styles.trackTitleImage}>
-                        <svg className={styles.trackTitleSvg}>
-                          <svg className={styles.trackTitleSvg}>
-                            <use xlinkHref="img/icon/sprite.svg#icon-note"></use>
-                          </svg>
-                        </svg>
-                      </div>
-                      <div className={styles.trackListText}>
-                        <a className={styles.trackListLink} href="#">
-                          {track.name}
-                          <span className={styles.trackTitleSpan}></span>
-                        </a>
-                      </div>
-                    </div>
-                    <div className={styles.trackAuthor}>
-                      <a className={styles.trackAuthorLink} href="#">
-                        {track.author}
-                      </a>
-                    </div>
-                    <div className={styles.trackAlbum}>
-                      <a className={styles.trackAlbumLink} href="#">
-                        {track.album}
-                      </a>
-                    </div>
-                    <div className={styles.trackTime}>
-                      <svg className={styles.trackTimeSvg}>
-                        <use xlinkHref="img/icon/sprite.svg#icon-like"></use>
+            {tracks.map((track) => (
+              <div className={styles.playlistItem} key={track.id}>
+                <div className={clsx(styles.playlistTrack, styles.track)}>
+                  <div className={styles.trackTitle}>
+                    <div className={styles.trackTitleImage}>
+                      <svg className={styles.trackTitleSvg}>
+                        <use xlinkHref="/img/icon/sprite.svg#icon-note"></use>
                       </svg>
-                      <span className={styles.trackTimeText}>4:44</span>
+                    </div>
+                    <div className={styles.trackListText}>
+                      <a className={styles.trackListLink} href="#">
+                        {track.name}
+                        <span className={styles.trackTitleSpan}></span>
+                      </a>
                     </div>
                   </div>
+                  <div className={styles.trackAuthor}>
+                    <a className={styles.trackAuthorLink} href="#">
+                      {track.author}
+                    </a>
+                  </div>
+                  <div className={styles.trackAlbum}>
+                    <a className={styles.trackAlbumLink} href="#">
+                      {track.album}
+                    </a>
+                  </div>
+                  <div className={styles.trackTime}>
+                    <svg className={styles.trackTimeSvg}>
+                      <use xlinkHref="/img/icon/sprite.svg#icon-like"></use>
+                    </svg>
+                    <span className={styles.trackTimeText}>
+                      {formatDuration(track.duration_in_seconds)}
+                    </span>
+                  </div>
                 </div>
-              );
-            })}
+              </div>
+            ))}
           </div>
         </div>
       </div>
--- src/components/Sorting/Sorting.tsx
+++ src/components/Sorting/Sorting.tsx
@@ -1,16 +1,37 @@
 "use client";
 import styles from "./Sorting.module.css";
 import clsx from "clsx";
-import { sortingData } from "./data";
-import { useState } from "react";
+import { FC, useState } from "react";
 import Filter from "../Filter/Filter";
+import { TrackType } from "@/types/types";
 
-export const Sorting = () => {
+type Props = {
+  tracks: TrackType[];
+};
+
+export const Sorting: FC<Props> = ({ tracks }) => {
   const [filterValue, setFilterValue] = useState<string | null>(null);
   const handledFilterValue = (value: string) => {
     setFilterValue((prev) => (prev === value ? null : value));
   };
 
+  const sortingData = [
+    {
+      title: "исполнителю",
+      list: Array.from(new Set(tracks.map((track) => track.author))),
+      value: "author",
+    },
+    {
+      title: "году выпуска",
+      list: ["По умолчанию", "Сначала новые", "Сначала старые"],
+      value: "release",
+    },
+    {
+      title: "жанру",
+      list: Array.from(new Set(tracks.map((track) => track.genre))),
+      value: "genre",
+    },
+  ];
   return (
     <div className={clsx(styles.centerBlockFilter, styles.filter)}>
       <div className={styles.filterTitle}>Искать по:</div>
