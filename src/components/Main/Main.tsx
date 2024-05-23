4df12600d0d3058e940a265722a732e80b0bc4ea
p1ck23 <danil-vetrov47@mail.ru>
2024-05-23 20:41:56 +0400

    relocate api get

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,14 +3,17 @@ import { Menu } from "../Menu/Menu";
 import { Sidebar } from "../Sidebar/Sidebar";
 import { SongList } from "../SongList/SongList";
 import { Player } from "../Player/Player";
+import { TrackType } from "@/types/types";
+import { getTracks } from "@/api/api";
 
 export const Main = async () => {
+  const tracks: TrackType[] = await getTracks();
   return (
     <div className={styles.wrapper}>
       <div className={styles.container}>
         <main className={styles.main}>
           <Menu />
-          <SongList />
+          <SongList tracks={tracks} />
           <Sidebar />
         </main>
         <Player />
