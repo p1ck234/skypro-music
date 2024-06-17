67bab8b68684d8722d93dd91ac2abd39b08e897e
p1ck23 <danil-vetrov47@mail.ru>
2024-06-17 16:42:05 +0400

    progressbar test

--- src/components/ProgressBar/ProgressBar.tsx
+++ src/components/ProgressBar/ProgressBar.tsx
@@ -8,19 +8,21 @@ type ProgressBarType = {
   onChange: (e: ChangeEvent<HTMLInputElement>) => void;
 };
 
-const ProgressBar = React.memo(({ max = 0, value, step, onChange }:ProgressBarType) => {
-  return (
-    <input
-      className={styles.styledProgressInput} // Применение стилей к ползунку
-      type="range" // Тип элемента - ползунок
-      min="0" // Минимальное значение ползунка
-      max={max} // Максимальное значение, зависит от длительности аудио
-      value={value} // Текущее значение ползунка
-      step={step} // Шаг изменения значения
-      onChange={onChange} // Обработчик события изменения
-    />
-  );
-});
+const ProgressBar = React.memo(
+  ({ max = 0, value, step, onChange }: ProgressBarType) => {
+    return (
+      <input
+        className={styles.styledProgressInput} // Применение стилей к ползунку
+        type="range" // Тип элемента - ползунок
+        min="0" // Минимальное значение ползунка
+        max={max} // Максимальное значение, зависит от длительности аудио
+        value={value} // Текущее значение ползунка
+        step={step} // Шаг изменения значения
+        onChange={onChange} // Обработчик события изменения
+      />
+    );
+  }
+);
 
-ProgressBar.displayName = "ProgressBar"
+ProgressBar.displayName = "ProgressBar";
 export default ProgressBar;
