7a905c6551cf1e23a946f0e12958d5bdec8bdf1a
p1ck23 <danil-vetrov47@mail.ru>
2024-06-20 22:46:10 +0400

    clear main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
-import Bar from "@/components/Bar/Bar";
-import Navigation from "@/components/Navigation/Navigation";
-import SideBar from "@/components/SideBar/SideBar";
-import CenterBlock from "@/components/CenterBlock/CenterBlock";
-import styles from "./page.module.css";
+// import Bar from "@/components/Bar/Bar";
+// import Navigation from "@/components/Navigation/Navigation";
+// import SideBar from "@/components/SideBar/SideBar";
+// import CenterBlock from "@/components/CenterBlock/CenterBlock";
+// import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.wrapper}>
-      <div className={styles.container}>
-        <main className={styles.main}>
-          <Navigation />
-          <CenterBlock />
-          <SideBar />
-        </main>
-        <Bar />
-        <footer className={styles.footer} />
-      </div>
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div className={styles.wrapper}>
+//       <div className={styles.container}>
+//         <main className={styles.main}>
+//           <Navigation />
+//           <CenterBlock />
+//           <SideBar />
+//         </main>
+//         <Bar />
+//         <footer className={styles.footer} />
+//       </div>
+//     </div>
+//   );
+// }
