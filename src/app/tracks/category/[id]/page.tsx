896cb4c1144954fa4f08c3da4e07a86a148f5a16
p1ck23 <danil-vetrov47@mail.ru>
2024-06-20 22:51:00 +0400

    setting layout category id

--- src/app/tracks/category/[id]/page.tsx
+++ src/app/tracks/category/[id]/page.tsx
@@ -1,15 +1,33 @@
+"use client";
 import { getPlaylistTracks } from "@/api/api";
 import CenterBlock from "@/components/CenterBlock/CenterBlock";
-
+import styles from "../../layout.module.css";
 type CategoryType = {
   params: { id: string };
 };
 
 export default async function CategoryPage({ params }: CategoryType) {
   const tracksData = await getPlaylistTracks(params.id);
+
+  let title = "";
+  switch (params.id) {
+    case "1": // if (x === 'value1')
+      title = "Плейлист дня";
+      break;
+
+    case "2": // if (x === 'value2')
+      title = "100 танцевальных хитов";
+      break;
+    case "3": // if (x === 'value2')
+      title = "Инди-заряд";
+      break;
+    default:
+      break;
+  }
   return (
-    <>
+    <div className={styles.mainCenterblock}>
+      <h2 className={styles.centerblockH2}>{title}</h2>
       <CenterBlock tracks={tracksData} playlist={tracksData} />
-    </>
+    </div>
   );
 }
