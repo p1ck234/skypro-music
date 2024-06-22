79461c744aa1792cdd5e2200bbb0976ffbebc77a
p1ck23 <danil-vetrov47@mail.ru>
2024-06-22 17:41:36 +0400

    exit component functional

--- src/components/Exit/Exit.tsx
+++ src/components/Exit/Exit.tsx
@@ -1,14 +1,40 @@
-import styles from "./Exit.module.css"
+"use client";
+import { useAppDispatch, useAppSelector } from "@/hooks/hooks";
+import styles from "./Exit.module.css";
+import { setAuthState, setUserData } from "@/store/features/authSlice";
+import { useEffect, useState } from "react";
 
 const Exit = () => {
+  const dispatch = useAppDispatch();
+  const [isHydrated, setIsHydrated] = useState(false); // Track hydration state
+  const logged = useAppSelector((state) => state.auth.authState);
+  const userName = useAppSelector((state) => state.auth.userData);
+
+  useEffect(() => {
+    setIsHydrated(true);
+  }, []);
+
+  const logout = () => {
+    dispatch(setAuthState(false));
+    dispatch(setUserData(null));
+    localStorage.removeItem("user");
+    localStorage.removeItem("token");
+  };
+
+  if (!isHydrated) {
+    return null;
+  }
+
   return (
     <div className={styles.sidebarPersonal}>
-      <p className={styles.sidebarPersonalName}>Sergey.Ivanov</p>
-      <div className={styles.sidebarIcon}>
-        <svg>
-          <use href="/img/icon/sprite.svg#logout" />
-        </svg>
-      </div>
+      <p className={styles.sidebarPersonalName}>{userName?.email}</p>
+      {logged && (
+        <div onClick={logout} className={styles.sidebarIcon}>
+          <svg>
+            <use xlinkHref="img/icon/sprite.svg#logout" />
+          </svg>
+        </div>
+      )}
     </div>
   );
 };
