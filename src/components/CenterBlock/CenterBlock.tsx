5b5f166c968d08518ef2660314f1f7c2c6df38df
p1ck23 <danil-vetrov47@mail.ru>
2024-06-22 17:41:59 +0400

    clear cb comp and isFavorite

--- src/components/CenterBlock/CenterBlock.tsx
+++ src/components/CenterBlock/CenterBlock.tsx
@@ -1,23 +1,19 @@
 "use client";
-import styles from "./CentarBlock.module.css";
-import Filters from "../Filters/Filters";
-import Search from "../Search/Search";
 import Playlist from "../Playlist/Playlist";
 import { TrackType } from "@/types/types";
 
 export default function CenterBlock({
   tracks,
   playlist,
+  isFavorite,
 }: {
   tracks: TrackType[];
   playlist: TrackType[];
+  isFavorite?: boolean;
 }) {
   return (
     <>
-      {/* <Search /> */}
-      {/* <h2 className={styles.centerblockH2}>Треки</h2>
-      <Filters /> */}
-      <Playlist tracks={tracks} playlist={playlist} />
+      <Playlist tracks={tracks} playlist={playlist} isFavorite={isFavorite} />
     </>
   );
 }
