0fccb034a8ee1cf5ab4613f6eeb04d640f943f6e
p1ck23 <danil-vetrov47@mail.ru>
2024-06-22 16:44:00 +0400

    authslice

--- src/hooks/hooks.ts
+++ src/hooks/hooks.ts
@@ -6,7 +6,6 @@ import {
 } from "react-redux";
 import { AppDispatch, AppStore, RootState } from "../store/store";
 
-// Хуки useAppDispatch, useAppSelector и useAppStore позволяют использовать функции useDispatch, useSelector и useStore из библиотеки react-redux с типизацией.
 export const useAppDispatch: () => AppDispatch = useDispatch;
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 export const useAppStore: () => AppStore = useStore;
